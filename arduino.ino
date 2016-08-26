4f330745020ca90573c85dfd047836e9a08e2416
Lineu Felipe <lineufelipe@gmail.com>
2016-08-25 23:52:56 -0300

    Adding send and receive IR and NET codes

--- arduino.ino
+++ arduino.ino
@@ -1,37 +1,63 @@
-#define W_SEND 'w'
-#define W_RECV 'W'
+#include <IRremote.h>
+#include <IRremoteInt.h>
 
-struct Wifi {
-    String name, pass;
-} wifi;
+#define ENTER 10
+#define NET_TV 11
 
 void setup();
 void loop();
 String *parse(String param, int *len);
 void command(String *params, int len);
 
+/****************************************/
+decode_results result, last;
+int bits = 32;
+unsigned long cmd_number[12];
+IRrecv irrecv(11);
+IRsend sendIR;
+
+/****************************************/
 
 /****************************************/
 void setup() {
+    cmd_number[0] = 3782869247;
+    cmd_number[1] = 3782901887;
+    cmd_number[2] = 3782885567;
+    cmd_number[3] = 3782918207;
+    cmd_number[4] = 3782877407;
+    cmd_number[5] = 3782910047;
+    cmd_number[6] = 3782893727;
+    cmd_number[7] = 3782926367;
+    cmd_number[8] = 3782873327;
+    cmd_number[9] = 3782905967;
+    cmd_number[10] = 3782912087;
+    cmd_number[11] = 3782879447;
     Serial.begin(9600);
+    irrecv.enableIRIn();
 }
 
 void loop() {
     delay(50);
-    if (Serial.available()) {
-        delay(50);
+    if (Serial.available() >= 10) {
         int n = Serial.available();
         char *tmp = new char[n];
         for (int i = 0; i < n; i++) {
             tmp[i] = Serial.read();
         }
         tmp[n] = '\0';
-        Serial.println(tmp);
 
         int l = 0;
         String *ret = parse(tmp, &l);
         command(ret, l);
         delete tmp, ret;
+    }
+
+    if (irrecv.decode(&result)) {
+        if (result.value != 0xFFFFFFFF) {
+            last = result;
+            Serial.println(result.value);
+        }
+        irrecv.resume();
     }
 }
 
@@ -56,6 +82,7 @@ String* parse(String param, int *len) {
     do {
         f = param.indexOf(';', f);
         ret[i] = param.substring(a, f);
+        ret[i].trim();
         if (f == -1)
             break;
         a = ++f;
@@ -68,14 +95,15 @@ String* parse(String param, int *len) {
 void command(String *params, int len) {
     String cmd = params[0];
     switch (cmd[0]) {
-    case 'w':
-        wifi.name = params[1];
-        wifi.pass = params[2];
-    case 'W':
-        Serial.print("w;");
-        Serial.print(wifi.name);
-        Serial.print(";");
-        Serial.println(wifi.pass);
+    case 'c':
+        Serial.print(params[1]);
+        for (int i = 0; i < params[1].length(); i++) {
+            int pos = params[1][i]-48;
+            sendIR.sendNEC(cmd_number[pos], 32);
+            delay(300);
+        }
+
+        irrecv.enableIRIn();
         break;
     default:
         Serial.print("Command not found: ");
